d24940857eca6c44d6b91bfc90579c9a606b2c1c
gerardodsc27@gmail.com <gerardodsc27@gmail.com>
2022-06-03 14:50:44 +0200

    Boids

---
+++ ShaderScenary/Assets/Scenes/Delivery2/Delivery2_Profiles/BoisAI/BoidAlignmetBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(Boid))]
+public class BoidAlignmetBehaviour : MonoBehaviour
+{
+
+    private Boid boid;
+    public float radius;
+
+    void Start()
+    {
+        boid = GetComponent<Boid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check the closest boids and acced their component
+        var boids = FindObjectsOfType<Boid>();
+        var average = Vector3.zero;
+        var found = 0;
+
+        foreach (var boid in boids.Where(b => b != boid))
+        {
+            var difference = boid.transform.position - this.transform.position;
+            if (difference.magnitude < radius)
+            {
+                average += boid.velocity;
+                found += 1;
+            }
+        }
+        //If there are some, move in this direction
+        if (found > 0)
+        {
+            average = average / found;
+            boid.velocity += Vector3.Lerp(boid.velocity, average, Time.deltaTime);
+
+        }
+    }
+}
---
+++ ShaderScenary/Assets/Scenes/Delivery2/Delivery2_Profiles/BoisAI/BoidInverseBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(Boid))]
+public class BoidInverseBehaviour : MonoBehaviour
+{
+    private Boid boid;
+    public float radius;
+
+    public float repulsionForce;
+
+    void Start()
+    {
+        boid = GetComponent<Boid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check the closest boids and acced their component
+        var boids = FindObjectsOfType<Boid>();
+        var average = Vector3.zero;
+        var found = 0;
+
+        foreach (var boid in boids.Where(b => b != boid))
+        {
+            var difference = boid.transform.position - this.transform.position;
+            if (difference.magnitude < radius)
+            {
+                average += difference;
+                found += 1;
+            }
+        }
+        //If there are some, respulse it 
+        if (found > 0)
+        {
+            average = average / found;
+            boid.velocity -= Vector3.Lerp(Vector3.zero, average, boid.velocity.magnitude / radius) * repulsionForce;
+
+        }
+    }
+}
